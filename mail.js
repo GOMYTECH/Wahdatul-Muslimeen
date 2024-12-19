68a85479abca8e3111f5ac989c4e07f91c33ced7
GOMY TECH <muharirajibola453@gmail.com>
2024-12-19 09:27:27 +0100

    Create mail.js

---
+++ mail.js
@@ -0,0 +1,28 @@
+  // Your web app's Firebase configuration
+
+
+  const firebaseConfig = {
+
+
+    apiKey: "AIzaSyBuZKsIywHs8BfYMCXjVmHzGTx7s29ZkBs",
+
+
+    authDomain: "timit-63209.firebaseapp.com",
+
+
+    databaseURL: "https://timit-63209-default-rtdb.firebaseio.com",
+
+
+    projectId: "timit-63209",
+
+
+    storageBucket: "timit-63209.firebasestorage.app",
+
+
+    messagingSenderId: "387541308403",
+
+
+    appId: "1:387541308403:web:2e2cbf2cdb56fb020df886"
+
+
+  };
